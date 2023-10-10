9f636427878109c12951bb1f00dd2a713168c009
Jordan Sim-Smith <jordansimsmith@canva.com>
2023-10-11 10:36:08 +1300

    Initialise a spring boot project

---
+++ src/main/java/com/jordansimsmith/immersion/tracker/ImmersionTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.jordansimsmith.immersion.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmersionTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmersionTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jordansimsmith/immersion/tracker/ImmersionTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordansimsmith.immersion.tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmersionTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
