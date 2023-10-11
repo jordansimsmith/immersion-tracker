9f607b706b88630682bed6fe2fea5325bb7407d8
Jordan Sim-Smith <jordansimsmith@canva.com>
2023-10-11 15:22:43 +1300

    Implement basic auth for the sync endpoint

---
+++ src/main/java/com/jordansimsmith/immersion/tracker/ImmersionTrackerConfiguration.java
@@ -0,0 +1,49 @@
+package com.jordansimsmith.immersion.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class ImmersionTrackerConfiguration {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(
+            @Value("${immersion.tracker.username}") String username,
+            @Value("${immersion.tracker.password}") String password,
+            PasswordEncoder passwordEncoder) {
+        var manager = new InMemoryUserDetailsManager();
+        var user =
+                User.builder()
+                        .username(username)
+                        .password(passwordEncoder.encode(password))
+                        .build();
+        manager.createUser(user);
+        return manager;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .securityMatcher("/sync")
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
