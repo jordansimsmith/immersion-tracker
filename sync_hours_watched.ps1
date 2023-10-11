dbf73d4ef0b6f990677e41a609aecd85999824d8
Jordan Sim-Smith <jordansimsmith@canva.com>
2023-10-11 17:55:58 +1300

    Create sync_hours_watched.ps1

---
+++ sync_hours_watched.ps1
@@ -0,0 +1,55 @@
+$SyncMessages = @()
+$Timestamp = (Get-Date).ToString("yyyy-MM-ddThh:mm:ss")
+$Username=""
+$Password=""
+
+Get-ChildItem -Directory |
+Foreach-Object {
+    $FolderName = $_.BaseName
+    $Path = Join-Path -Path $_.FullName -ChildPath "watched"
+
+    if (!(Test-Path -LiteralPath $Path))
+    {
+        return
+    }
+
+    Get-ChildItem -LiteralPath $Path |
+    Foreach-Object {
+        $FileName = $_.BaseName
+        $SyncMessage = @{
+            folder_name = $FolderName
+            file_name = $FileName
+            timestamp = $Timestamp
+        }
+        $SyncMessages += $SyncMessage    
+    }
+}
+
+$Authorization = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $Username, $Password)))
+
+$Headers = @{
+    "Authorization" = "Basic " + $Authorization
+    "Content-Type" = "application/json"
+}
+
+$Body = $SyncMessages | ConvertTo-Json
+
+Invoke-RestMethod -Uri "https://immersion-tracker.jordansimsmith.com/sync" -Method Post -Headers $Headers -Body $Body
+
+$Progress = Invoke-RestMethod -Uri "https://immersion-tracker.jordansimsmith.com/progress"
+$Progress.episodes_per_show_watched.PsObject.Properties |
+Foreach-Object {
+    $DisplayName = $_.Name -replace "\[[^\]]*\]\s?" -replace "\([^\)]*\)\s?"
+    Write-Host $_.Value "episodes of" $DisplayName
+}
+
+Write-Host
+Write-Host $Progress.total_hours_watched "total hours watched"
+
+
+if ($Host.Name -eq "ConsoleHost")
+{
+    Write-Host
+    Write-Host "Press any key to continue..."
+    $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+}
