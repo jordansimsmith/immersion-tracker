744133715f2757509a4e6f4c951983b61fe8f6a9
Jordan Sim-Smith <jordansimsmith@canva.com>
2023-11-05 11:00:24 +1300

    Implement the chart endpoint to visualise accumulation of episodes watched

--- src/main/java/com/jordansimsmith/immersion/tracker/ImmersionTrackerController.java
+++ src/main/java/com/jordansimsmith/immersion/tracker/ImmersionTrackerController.java
@@ -3,15 +3,25 @@ package com.jordansimsmith.immersion.tracker;
 import static com.jordansimsmith.immersion.tracker.jooq.Tables.EPISODE;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.awt.*;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtils;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
 import org.jooq.Configuration;
 import org.jooq.DSLContext;
+import org.jooq.DatePart;
 import org.jooq.Record2;
 import org.jooq.impl.DSL;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -56,6 +66,49 @@ public class ImmersionTrackerController {
                 .forEach(s -> episodesPerShowWatched.put(s.value1(), s.value2()));
 
         return new ProgressMessage(totalEpisodesWatched, totalHoursWatched, episodesPerShowWatched);
+    }
+
+    @GetMapping(value = "/chart", produces = MediaType.IMAGE_PNG_VALUE)
+    public byte[] chart() throws IOException {
+        var year = DSL.extract(EPISODE.TIMESTAMP, DatePart.YEAR).as("year");
+        var month = DSL.extract(EPISODE.TIMESTAMP, DatePart.MONTH).as("month");
+        var episodesPerMonth =
+                create.select(year, month, DSL.count().as("episodes_watched"))
+                        .from(EPISODE)
+                        .groupBy(year, month)
+                        .orderBy(year.asc(), month.asc())
+                        .fetch();
+
+        var dataset = new DefaultCategoryDataset();
+        // immersion start date
+        dataset.addValue(0, "episodes", LocalDate.of(2023, 5, 1));
+        var sum = 0;
+        for (var episodes : episodesPerMonth) {
+            sum += episodes.value3();
+            var date = LocalDate.of(episodes.value1(), episodes.value2(), 1);
+            dataset.addValue(sum, "episodes", date);
+        }
+
+        var chart =
+                ChartFactory.createLineChart(
+                        "accumulation of episodes watched",
+                        "month",
+                        "episodes watched",
+                        dataset,
+                        PlotOrientation.VERTICAL,
+                        false,
+                        false,
+                        false);
+        var plot = chart.getCategoryPlot();
+        plot.setBackgroundPaint(Color.white);
+        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+        plot.setOutlineVisible(false);
+
+        try (var outputStream = new ByteArrayOutputStream()) {
+            ChartUtils.writeChartAsPNG(outputStream, chart, 800, 600);
+            return outputStream.toByteArray();
+        }
     }
 
     @PostMapping("/sync")
