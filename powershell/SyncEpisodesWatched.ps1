6643328a19113b62908aecc56cd3a1c43256911b
Jordan Sim-Smith <jordansimsmith@gmail.com>
2024-02-27 12:08:40 +1300

    Add version number bumping to workflow

--- powershell/SyncEpisodesWatched.ps1
+++ powershell/SyncEpisodesWatched.ps1
@@ -1,4 +1,10 @@
-Install-Module -Name 'ImmersionTracker'
-Update-Module -Name 'ImmersionTracker'
+if (-Not (Get-Module -ListAvailable -Name ImmersionTracker)) {
+    Write-Host "installing module..."
+    Install-Module -Name 'ImmersionTracker'
+}
+else {
+    Write-Host "checking for updates..."
+    Update-Module -Name 'ImmersionTracker'
+}
 
 Sync-Episodes-Watched
